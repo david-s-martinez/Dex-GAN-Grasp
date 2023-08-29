ff2013f652bb236eb9b4b32d17b8b0b1271bd76a
David S. Martinez <56695799+davidmartinez13@users.noreply.github.com>
2023-08-30 01:29:23 +0200

    added los functions template for GAN

--- gan_test.py
+++ gan_test.py
@@ -1,7 +1,7 @@
 from __future__ import division
 
 import os
-
+import time
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
@@ -13,10 +13,10 @@ from FFHNet.data.ffhcollision_data_set import FFHCollDetrDataSet
 from FFHNet.data.ffhevaluator_data_set import (FFHEvaluatorDataSet,
                                                FFHEvaluatorPCDDataSet)
 from FFHNet.data.ffhgenerator_data_set import FFHGeneratorDataSet
-from FFHNet.models.ffhnet import FFHNet
+from FFHNet.models.ffhgan import FFHGANet
 from FFHNet.models.networks import FFHGAN
 from FFHNet.utils import utils, visualization, writer
-
+from FFHNet.utils.writer import Writer
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 BASE_PATH = os.path.split(os.path.split(ROOT_PATH)[0])[0]
 parser = argparse.ArgumentParser(
@@ -80,6 +80,94 @@ def eval_ffhnet_sampling_and_filtering_real(config_path,
         #         a = input('Break loop? (y/n): ')
         #         if a == 'y':
         #             break
+def train():
+    parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--config', help='Path to template image.',
+                        default='FFHNet/config/config_ffhnet_vm_test.yaml')
+    args = parser.parse_args()
+
+    # load configuration params
+    config = Config(args.config)
+    cfg = config.parse()
+
+    # start cuda multiprocessing
+    # TODO: discover the problem of cpu usage
+    torch.multiprocessing.set_start_method('spawn')
+
+    # Data for gen and eval and col is different. Gen sees only positive examples
+    if cfg["train_ffhevaluator"]:
+        if cfg["model"] == "ffhnet":
+            dset_eva = FFHEvaluatorDataSet(cfg,eval=False)
+        elif cfg["model"] == "pointnet":
+            dset_eva = FFHEvaluatorPCDDataSet(cfg,eval=False)
+        train_loader_eva = DataLoader(dset_eva,
+                                      batch_size=cfg["batch_size"],
+                                      shuffle=True,
+                                      drop_last=True,
+                                      num_workers=cfg["num_threads"])
+
+    if cfg["train_ffhgenerator"]:
+        dset_gen = FFHGeneratorDataSet(cfg)
+        train_loader_gen = DataLoader(dset_gen,
+                                      batch_size=cfg["batch_size"],
+                                      shuffle=True,
+                                      drop_last=True,
+                                      num_workers=cfg["num_threads"])
+    if cfg["train_ffhcolldetr"]:
+        dset_eva = FFHCollDetrDataSet(cfg)
+        train_loader_col = DataLoader(dset_eva,
+                                      batch_size=cfg["batch_size"],
+                                      shuffle=True,
+                                      drop_last=True,
+                                      num_workers=cfg["num_threads"])
+
+    writer = Writer(cfg)
+
+    ffhgan = FFHGANet(cfg)
+    # if cfg["continue_train"]:
+    #     if cfg["train_ffhevaluator"]:
+    #         ffhgan.load_ffhevaluator(cfg["load_epoch"])
+    #     if cfg["train_ffhgenerator"]:
+    #         ffhgan.load_ffhgenerator(cfg["load_epoch"])
+    #     if cfg["train_ffhcolldetr"]:
+    #         ffhgan.load_ffhcolldetr(cfg["load_epoch"])
+    #     start_epoch = cfg["load_epoch"] + 1
+    # else:
+    #     start_epoch = 1
+    start_epoch = 1
+    total_steps = 0
+    epoch_start = time.time()
+
+    for epoch in range(start_epoch, cfg["num_epochs"] + 1):
+        # === Generator ===
+        
+        # Initialize epoch / iter info
+        prev_iter_end = time.time()
+        epoch_iter = 0
+
+        for i, data in enumerate(train_loader_gen):
+            # Update iter and total info
+            cur_iter_start = time.time()
+            total_steps += cfg["batch_size"]
+            epoch_iter += cfg["batch_size"]
+
+            # Measure time for data loading
+            if total_steps % cfg["print_freq"] == 0:
+                t_load_data = cur_iter_start - prev_iter_end
+
+            # Update model one step, get losses
+            loss_dict = ffhgan.update_ffhgan(data)
+
+            # Log loss
+            if total_steps % cfg["print_freq"] == 0:
+                t_load = cur_iter_start - prev_iter_end  # time for data loading
+                t_total = (time.time() - cur_iter_start) // 60
+                writer.print_current_train_loss(epoch, epoch_iter, loss_dict, t_total, t_load)
+                writer.plot_train_loss(loss_dict, epoch, epoch_iter, len(dset_gen))
+
+            prev_iter_end = time.time()
+            # End of data loading generator
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
