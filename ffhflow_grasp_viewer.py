8e8ae24df4925a76f89377b6b32be594e0b0a378
davidmartinez13 <dsmartinez13@hotmail.com>
2024-06-28 19:50:54 +0200

    feat: added real world inference

---
+++ ffhflow_grasp_viewer.py
@@ -0,0 +1,139 @@
+
+# Run this in FFHNet env
+import os
+import sys
+sys.path.append('/home/dm/panda_ws/inference_container/Multifinger-Net-dev')
+
+import numpy as np
+import pyrender
+from FFHNet.utils import utils
+from urdfpy import URDF
+from copy import deepcopy
+import trimesh
+
+BASE_PATH = '/home/qian.feng/FFHNet-dev'
+def show_grasp_and_object_given_pcd(pcd, centr_T_palm, joint_conf):
+    """Visualize the grasp object and the hand relative to it. Here the pcd is already transformed to grasp center
+
+    Args:
+        pcd: point cloud i open3d format, in world/base frame
+        centr_T_palm (4*4 array): Homogeneous transform that describes the grasp (palm pose) w.r.t to point cloud centroid.
+        joint_conf (15 or 20*1 array): 15 or 20 dimensional joint configuration
+    """
+    # path = os.path.dirname(os.path.abspath(__file__))
+    # BASE_PATH = os.path.split(path)[0]
+
+    robot = URDF.load(os.path.join(
+        BASE_PATH, 'meshes/hithand_palm/hithand.urdf'))
+
+    # get the full joint config
+    if joint_conf.shape[0] == 15:
+        joint_conf_full = utils.full_joint_conf_from_partial_joint_conf(
+            joint_conf)
+    elif joint_conf.shape[0] == 20:
+        joint_conf_full = joint_conf
+    else:
+        raise Exception('Joint_conf has the wrong size in dimension one: %d. Should be 15 or 20' %
+                        joint_conf.shape[0])
+    # if you want to show hand pregrasp pose with finger straight.
+    joint_conf_full = np.zeros(20)
+    cfg_map = utils.get_hand_cfg_map(joint_conf_full)
+
+    # compute fk for meshes and links
+    fk = robot.visual_trimesh_fk(cfg=cfg_map)
+    fk_link = robot.link_fk()
+    assert robot.links[2].name == 'palm_link_hithand'  # link 2 must be palm
+    # get the transform from base to palm
+    hand_base_T_palm = fk_link[robot.links[2]]
+
+    # Compute the transform from base to object centroid frame
+    palm_T_centr = np.linalg.inv(centr_T_palm)
+    hand_base_T_centr = np.matmul(hand_base_T_palm, palm_T_centr)
+
+    # Turn open3d pcd into pyrender mesh or load trimesh from path
+    pcd_center_T_world = np.eye(4)
+    pcd_center_T_world[:3,-1] = -1*pcd.get_center()
+    copy_pcd = deepcopy(pcd)
+    copy_pcd.translate(-1*pcd.get_center())
+    centr_T_hand_base = np.linalg.inv(hand_base_T_centr)
+    pcd_centr_T_hand_base = np.matmul(pcd_center_T_world, centr_T_hand_base)
+
+    pts = np.asarray(copy_pcd.points)
+
+    # obj_geometry = pyrender.Mesh.from_points(pts,
+    #                                              colors=np.tile([55, 55, 4], (pts.shape[0], 1)))
+    # obj_geometry = pyrender.Mesh.from_points(pts,
+    #                                              colors=np.asarray(copy_pcd.colors))
+
+    sm = trimesh.creation.uv_sphere(radius=0.001)
+    sm.visual.vertex_colors = [1.0, 0.0, 0.0]
+    tfs = np.tile(np.eye(4), (len(pts), 1, 1))
+    tfs[:,:3,3] = pts
+    obj_geometry = pyrender.Mesh.from_trimesh(sm, poses=tfs)
+
+    # Construct a scene
+    scene = pyrender.Scene(bg_color=(255,255,255))
+    # scene = pyrender.Scene(bg_color=(1,1,1))
+
+    # Translate everything to pcd center
+    # Add the robot to the scene
+    for tm in fk:
+        pose = fk[tm]
+        pose = np.matmul(pcd_centr_T_hand_base, pose)
+        mesh = pyrender.Mesh.from_trimesh(tm, smooth=False)
+        scene.add(mesh, pose=pose)
+
+    # Add cloud to scene
+    # centr_T_base
+    scene.add(obj_geometry, pose=np.eye(4))
+
+    # Add more light to scene
+    pose_light = np.eye(4)
+    pose_light[:3, 3] = [-0.5, 0, 0]
+    scene.add(pyrender.PointLight(intensity=10), pose=pose_light)
+    pose_light = np.eye(4)
+    pose_light[:3, 3] = [0.5, 0, 0]
+    scene.add(pyrender.PointLight(intensity=10), pose=pose_light)
+    pose_light = np.eye(4)
+    pose_light[:3, 3] = [0, 0.9, 0]
+    scene.add(pyrender.PointLight(intensity=10), pose=pose_light)
+    pose_light = np.eye(4)
+    pose_light[:3, 3] = [0, -0.9, 0]
+    scene.add(pyrender.PointLight(intensity=10), pose=pose_light)
+
+    T_view_1 = np.array([[0.38758592, 0.19613444, -0.90072662, -0.54629509],
+                         [0.34160963, -0.93809507, -0.05727561, -0.12045398],
+                         [-0.85620091, -0.28549766, -0.43059386, -0.25333053], [0., 0., 0., 1.]])
+    T_view_2 = np.array([[0.38043475, 0.20440112, -0.90193658, -0.48869244],
+                         [0.36146523, -0.93055351, -0.05842123, -0.11668246],
+                         [-0.85124161, -0.30379325, -0.4278988, -0.22640526], [0., 0., 0., 1.]])
+
+    # View the scene
+    cam = pyrender.PerspectiveCamera(yfov=np.pi / 3.0)
+    # print(scene.scale)
+    nc = pyrender.Node(camera=cam, matrix=T_view_2)
+    scene.add_node(nc)
+
+    viewer = pyrender.Viewer(scene, viewer_flags={
+                    "fullscreen": False}, use_raymond_lighting=True)
+
+
+if __name__ == '__main__':
+    import numpy as np
+    import open3d as o3d
+    # pcd_np = np.load('/home/yb/Documents/ffhflow_grasp/pcd_path.npy')
+    # pcd = o3d.io.read_point_cloud(str(pcd_np))
+    # centr_T_palm = np.load('/home/yb/Documents/ffhflow_grasp/centr_T_palm.npy')
+    # centr_T_palm[-1,-1] = 1
+    # joint_conf = np.zeros(15)
+    # show_grasp_and_object_given_pcd(pcd, centr_T_palm, joint_conf)
+
+
+    data_path = '/home/dm/panda_ws/inference_container/Multifinger-Net-dev/data/real_objects/object'
+    pcd_path = os.path.join(data_path, 'cheez_it.pcd')
+    pcd = o3d.io.read_point_cloud(pcd_path)
+
+    grasp_path = os.path.join(data_path, 'grasp0000.npy')
+    centr_T_palm = np.load(grasp_path)
+    joint_conf = np.zeros(15)
+    show_grasp_and_object_given_pcd(pcd, centr_T_palm, joint_conf)
---
+++ ffhgan_inference.py
@@ -0,0 +1,234 @@
+import sys
+import os
+
+from copy import deepcopy
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+import math
+import time
+import logging
+import open3d as o3d
+import copy
+import cv2
+import torch
+import argparse
+from FFHNet.config.config import Config
+from FFHNet.data.bps_encoder import BPSEncoder
+from FFHNet.data.ffhevaluator_data_set import (FFHEvaluatorDataSet,
+                                               FFHEvaluatorPCDDataSet)
+from FFHNet.data.ffhgenerator_data_set import FFHGeneratorDataSet
+from FFHNet.models.ffhgan import FFHGANet
+from FFHNet.models.networks import FFHGAN
+from FFHNet.utils import utils, visualization, writer
+from FFHNet.utils.writer import Writer
+import bps_torch.bps as b_torch
+# from bps_torch.utils import to_np
+# Add GraspInference to the path and import
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..','src'))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+
+from segmentation import PlaneSegmentation
+from realsense import RealSense
+
+# Data collection
+# from ffhflow_grasp_viewer import show_grasp_and_object_given_pcd
+
+import zmq
+import numpy as np
+import open3d as o3d
+from time import time,sleep
+
+# context = zmq.Context()
+# socket = context.socket(zmq.REQ)
+# socket.connect("tcp://10.3.100.77:5561")
+# flags=0
+# track=False
+
+
+# def send_grasp(grasp_np):
+#     """
+#     obj_pcd_np in camera orientation but self centered.
+#     """
+
+#     info_seg = dict(
+#         grasp_np_dtype=str(grasp_np.dtype),
+#         ograsp_np_shape=grasp_np.shape,
+#     )
+#     start = time()
+#     socket.send_json(info_seg, flags | zmq.SNDMORE)
+#     socket.send_multipart([grasp_np], flags | zmq.SNDMORE, copy=True, track=track)
+
+#     # received_info_seg = socket.recv_json(flags=flags)
+#     # response_seg = socket.recv_multipart()
+
+#     # print('run ffhnet takes', time()-start)
+#     # grasp_poses_buffer = memoryview(response_seg[1])
+#     # grasp_poses = np.frombuffer(grasp_poses_buffer, dtype=received_info_seg["grasp_poses_dtype"]).reshape(received_info_seg["grasp_poses_shape"])
+#     # print("The reply is: ", grasp_poses.shape)
+
+#     return True
+
+
+def vis_all_grasps(pcd,cam_T_grasps_np):
+    obj_pcd_base = pcd.transform(base_T_cam)
+    grasps_to_show = []
+    for j in range(cam_T_grasps_np.shape[0]):
+        cam_T_grasp_np = cam_T_grasps_np[j,:4,:]
+        base_T_palm_np = np.matmul(base_T_cam, cam_T_grasp_np)
+        grasp_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.03)
+        grasp_frame = grasp_frame.transform(base_T_palm_np)
+        grasps_to_show.append(grasp_frame)
+    grasps_to_show.append(obj_pcd_base)
+
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    for f in grasps_to_show:
+        vis.add_geometry(f)
+    vis.run()
+
+
+# Transformation from flange frame to hand palm framer
+
+# camera
+save_path = '~/Downloads'
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+rs = RealSense(logger, save_path)
+segment = PlaneSegmentation()
+
+grasp_region_mask = np.zeros((720,1280),dtype=np.bool)
+# grasp_region_mask[150:420, 150:600] = True  # single obj
+grasp_region_mask[280:480, 650:950] = True  # cupboard grasping
+
+base_T_cam = np.array([[ 0.99993021, -0.00887332 ,-0.00779972 , 0.31846705],
+                    [ 0.00500804, -0.2795885  , 0.96010686 ,-1.10184744],
+                    [-0.01070005, -0.96007892 ,-0.27952455 , 0.50819482],
+                    [ 0.        ,  0.         , 0.          ,1.        ]])
+
+inter_offset = np.array([0.15, 0, 0])
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+BASE_PATH = os.path.split(os.path.split(ROOT_PATH)[0])[0]
+parser = argparse.ArgumentParser()
+# # Best VAE so far:
+# gen_path = "checkpoints/ffhnet/2023-09-01T01_16_11_ffhnet_lr_0.0001_bs_1000"
+# best_epoch = 24
+
+# Best GAN so far:
+gen_path = "checkpoints/ffhgan/2024-03-10T17_31_55_ffhgan_lr_0.0001_bs_1000"
+best_epoch = 32
+parser.add_argument('--gen_path', default=gen_path, help='path to FFHGenerator model')
+parser.add_argument('--load_gen_epoch', type=int, default=best_epoch, help='epoch of FFHGenerator model')
+# New evaluator:checkpoints/ffhevaluator/2024-06-23_ffhevaluator
+parser.add_argument('--eva_path', default='checkpoints/ffhevaluator/2024-06-23_ffhevaluator', help='path to FFHEvaluator model')
+parser.add_argument('--load_eva_epoch', type=int, default=30, help='epoch of FFHEvaluator model')
+parser.add_argument('--config', type=str, default='FFHNet/config/config_ffhgan.yaml')
+
+args = parser.parse_args()
+
+load_path_gen = args.gen_path
+load_path_eva = args.eva_path
+load_epoch_gen = args.load_gen_epoch
+load_epoch_eva = args.load_eva_epoch
+config_path = args.config
+config = Config(config_path)
+cfg = config.parse()
+ffhgan = FFHGANet(cfg)
+print(ffhgan)
+base_data_bath = os.path.join(ROOT_PATH,'data','real_objects')
+ffhgan.load_ffhgenerator(epoch=load_epoch_gen, load_path=load_path_gen)
+ffhgan.load_ffhevaluator(epoch=load_epoch_eva, load_path=load_path_eva)
+path_real_objs_bps = os.path.join(base_data_bath, 'bps')
+
+bps_path = './basis_point_set.npy'
+bps_np = np.load(bps_path)
+bps = b_torch.bps_torch(custom_basis=bps_np)
+
+
+i = int(input('i=?'))
+try:
+    while True:
+        color_image, depth_image, pcd, _ = rs.capture_image()
+        rs.visualize_color(color_image)
+        rs.visualize_depth(depth_image)
+        pcd_raw = deepcopy(pcd)
+        pcd_raw = rs.point_cloud_distance_removal_by_input(pcd_raw)
+        cv2.imshow("Display window", color_image)
+        k = cv2.waitKey(1)
+        print(f'get {i}th frame')
+        if k == 27:
+            break
+
+        pcd = segment.crop_pcd_with_bbox(pcd, grasp_region_mask)
+        pcd = rs.point_cloud_distance_removal(pcd)
+        obj_pcd, normal_vector = segment.plane_seg_with_angle_constrain(pcd)
+        # rs.save_images(i, color_image, depth_image, pcd, obj_pcd)
+
+        # crop depth in robot base with z > 0
+        crop_pcd = copy.deepcopy(obj_pcd).transform(base_T_cam)
+        crop_pcd_np = np.asarray(crop_pcd.points)
+        crop_pcd_np = crop_pcd_np[crop_pcd_np[:,2] >0]
+        crop_pcd = o3d.geometry.PointCloud()
+        crop_pcd.points = o3d.utility.Vector3dVector(crop_pcd_np)
+        # o3d.visualization.draw_geometries([crop_pcd])
+        obj_pcd = copy.deepcopy(crop_pcd).transform(np.linalg.inv(base_T_cam))
+
+        obj_pcd_cam = deepcopy(obj_pcd)
+
+        ####################### Run Inference  ##############
+        pcd_base = copy.deepcopy(pcd_raw).transform(base_T_cam)
+        obj_pcd_np = np.asarray(obj_pcd.points)
+        pcd_np = np.asarray(pcd.points)
+        pc_center = obj_pcd.get_center()
+        obj_pcd.translate(-pc_center)
+        points = np.asarray(obj_pcd.points)
+
+        pc_tensor = torch.from_numpy(points)
+        pc_tensor.to('cuda')
+        enc_dict = bps.encode(pc_tensor)
+
+        enc_np = enc_dict['dists'].cpu().detach().numpy()
+
+        grasps = ffhgan.generate_grasps(enc_np, n_samples=400, return_arr=True)
+        print(grasps)
+
+        # # # Visualize sampled distribution
+        obj_pcd_path = './obj.pcd'
+        o3d.io.write_point_cloud(obj_pcd_path, obj_pcd)
+        visualization.show_generated_grasp_distribution(obj_pcd_path, grasps)
+        filtered_grasps_2 = ffhgan.filter_grasps(enc_np, grasps, thresh=0.90)
+        n_grasps_filt_2 = filtered_grasps_2['rot_matrix'].shape[0]
+
+        print("n_grasps after filtering: %d" % n_grasps_filt_2)
+        print("This means %.2f of grasps pass the filtering" % (n_grasps_filt_2 / 400))
+
+        # Visulize filtered distribution
+        visualization.show_generated_grasp_distribution(obj_pcd_path, filtered_grasps_2)
+
+        if True:
+            for j in range(n_grasps_filt_2):
+                # Get the grasp sample
+                rot_matrix = filtered_grasps_2['rot_matrix'][j, :, :]
+                transl = filtered_grasps_2['transl'][j, :]
+                # if transl[1] > -0.1:
+                #     continue
+                joint_conf = filtered_grasps_2['joint_conf'][j, :]
+                palm_pose_centr = utils.hom_matrix_from_transl_rot_matrix(transl, rot_matrix)
+                visualization.show_grasp_and_object(obj_pcd_path, palm_pose_centr, joint_conf,
+                                                    'meshes/robotiq_palm/robotiq-3f-gripper_articulated.urdf')
+                a = input('Break loop? (y/n): ')
+                if a == 'y':
+                    break
+        # # TODO: add inference locally
+        # # cam_T_grasps_np = send_pcd(obj_pcd_np,pcd_np)
+        # print('got reply fro zeromq')
+
+        # # vis_all_grasps(pcd_raw,cam_T_grasps_np)
+
+        # ##### send grasp to local pc
+        # # send_grasp(cam_T_grasps_np)
+
+except KeyboardInterrupt:
+    print('something broke')
+# finally:
+#     socket.close()
+#     context.term()
